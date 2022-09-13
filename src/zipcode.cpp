d445330dd0d75996745ac53bd0832473414946c5
hi2k1978 <hi2k1978@gmail.com>
2022-09-14 00:56:15 +0900

    modify zipcode.cpp

--- src/zipcode.cpp
+++ src/zipcode.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 #define FMT_HEADER_ONLY
-#include<fmt/core.h>
+#include<fmt/format.h>
 
 #include<nlohmann/json.hpp>
 
@@ -19,56 +19,77 @@ using namespace Poco::Net;
 using namespace Poco;
 using json = nlohmann::json;
 
-int main() {
-  try{
-    const string zipcode = "1500032";
-    const string url = fmt::format("http://zipcloud.ibsnet.co.jp/api/search?zipcode={}", zipcode);
+struct AddressInquiryResult {
+  bool success;
+  string zipcode;
+  string address;
+};
 
-    unique_ptr<URI> moge;
-    cout << fmt::format("url={}, zipcode={}",url, zipcode) << endl;
+class AddressInquiry {
+  const string inquiry_url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=";
+  AddressInquiryResult failed {false, "", ""};
 
-    URI uri(url);    
-    HTTPClientSession session(uri.getHost(), uri.getPort());
-
-    string path(uri.getPathAndQuery());
-    if (path.empty()){
-      error_code ec = make_error_code(errc::network_down);
-      throw system_error{ec};
-    }
-
-    HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
-    session.sendRequest(req);
-
-    HTTPResponse res;
-    const int status = res.getStatus();
-    const string reason = res.getReason();
-    // cout << fmt::format("{}, {}", status, reason)<<endl;
-
-    if (200 <= status && status <= 299){
-      istream &is = session.receiveResponse(res);
-      string data, buf;
-      while(is >> buf){
-	// cout << fmt::format("buf = {}", buf) << endl;
-	data += buf;
+public:
+  AddressInquiry(){}
+  AddressInquiryResult search(string zipcode){
+    try{
+      const string url = inquiry_url + zipcode;
+      
+      URI uri(url);    
+      HTTPClientSession session(uri.getHost(), uri.getPort());
+      
+      string path(uri.getPathAndQuery());
+      if (path.empty()){
+	error_code ec = make_error_code(errc::network_down);
+	throw system_error{ec};
       }
-      // cout << fmt::format("data = {}", data) << endl;
-      json jdata = json::parse(data);
-      string result = fmt::format("{}{}{}",
-				  jdata["results"][0]["address1"],
-				  jdata["results"][0]["address2"],
-				  jdata["results"][0]["address3"]);
-      cout << fmt::format("result = {}", result) << endl;;
-    }
-  }catch(system_error &ex){
-    error_code ec = ex.code();
-    cout << ec.message() << endl;
-    return -1;
-  }catch(Exception &ex){
-    cerr << ex.displayText() << endl;
-    return -1;
+
+      HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+      session.sendRequest(req);
+
+      HTTPResponse res;
+      const int status = res.getStatus();
+      const string reason = res.getReason();
+      // cout << fmt::format("{}, {}", status, reason)<<endl;
+      if (200 <= status && status <= 299){
+	istream &is = session.receiveResponse(res);
+	string data, buf;
+	while(is >> buf){
+	  data += buf;
+	}
+	// cout << fmt::format("data = {}", data) << endl;
+	json jdata = json::parse(data);
+	string address = fmt::format("{}{}{}",
+				     jdata["results"][0]["address1"],
+				     jdata["results"][0]["address2"],
+				     jdata["results"][0]["address3"]);
+	AddressInquiryResult result {true, zipcode, address};
+	return result;
+      }
+      return failed;
+
+    }catch(system_error &ex){
+      error_code ec = ex.code();
+      cerr << ec.message() << endl;
+      return failed;
+    }catch(Exception &ex){
+      cerr << ex.displayText() << endl;
+      return failed;
+    };
+  }
+};
+
+int main() {
+  auto inquiry = make_unique<AddressInquiry>();
+  string address_list[] = {"1500032", "4040001", "0010013"};
+
+  cout << endl << "address_list:" << endl;
+  for (auto address: address_list){
+    AddressInquiryResult result = inquiry->search(address);
+    cout << fmt::format("{} -> {}", result.zipcode, result.address) << endl;
   }
   return 0;
-  
+ 
 }
 
 
