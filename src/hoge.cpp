0d60325d3acd2580fba1acb2d4a4fb505dcb8f42
hi2k1978 <hi2k1978@gmail.com>
2022-09-14 07:54:01 +0900

    modify

---
+++ src/hoge.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define  FMT_HEADER_ONLY
+#include<fmt/core.h>
+
+using namespace std;
+using fmt::format;
+
+template<typename T1, typename T2>
+auto moge(T1 x, T2 y) -> decltype(x+y) {
+  return x + y;
+}
+int main() {
+  auto xx = 3;
+  auto yy = 8.0;
+  auto zz = moge(xx, yy);
+  cout << format("zz={}", zz) << endl;
+  cout << format("zz={}", static_cast<char>(65)) << endl;
+  cout << R"(ho"ge)" << endl;
+  return 0;
+}
