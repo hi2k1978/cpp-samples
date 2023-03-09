bc512abbcd6754fecae4ebcf8732a2b78a964f98
hi2k1978 <hi2k1978@gmail.com>
2023-03-10 00:10:14 +0900

    refactor

--- lambda/src/CppSampleGet/handler.cpp
+++ lambda/src/CppSampleGet/handler.cpp
@@ -2,11 +2,12 @@
 #include <aws/core/utils/json/JsonSerializer.h>
 #include <aws/core/utils/memory/stl/SimpleStringStream.h>
 #include<memory>
+#include<iostream>
 
 #include "response.h"
 
 using namespace aws::lambda_runtime;
-
+using namespace aws::http;
 
 invocation_response lambda_handler(invocation_request const& request)
 {
@@ -52,15 +53,13 @@ invocation_response lambda_handler(invocation_request const& request)
     }
 
 
-    JsonValue body, bodyJsonValue;
-
-    body.WithString("statusCode", "200");
-
-    bodyJsonValue.WithString("message", "ok");
-    bodyJsonValue.WithString("result", ss.str());
-    body.WithString("body", bodyJsonValue.View().WriteCompact());
-
-    auto response = std::make_unique<CppLambda::Response>(std::move(body));
+    // int status_code = 201;
+    int status_code = 200;
+    JsonValue body;
+    body.WithString("message", "ok-bokuju");
+    body.WithString("result", ss.str());
+    
+    auto response = std::make_unique<CppLambda::Response>(std::move(status_code), std::move(body));
     return response->get();
 }
 
--- lambda/src/lib/response.cpp
+++ lambda/src/lib/response.cpp
@@ -1,18 +1,27 @@
 #include <aws/lambda-runtime/runtime.h>
 #include <aws/core/utils/json/JsonSerializer.h>
-
+#include <sstream>
 #include "response.h"
 
 using namespace aws::lambda_runtime;
+using namespace aws::http;
 
 namespace CppLambda {
 
     using namespace Aws::Utils::Json;
     
-    Response::Response(JsonValue&& body_) : body(std::move(body_)) {}
+    Response::Response(int status_code_, JsonValue body_)
+	: status_code(std::move(status_code_)), body(std::move(body_)) {}
 
     invocation_response Response::get() const {
-	return invocation_response::success(body.View().WriteCompact(), CONTENT_TYPE_JSON);	
+	
+	std::ostringstream status_code_stream;
+	status_code_stream << status_code;
+	
+	JsonValue response;
+	response.WithString("statusCode", status_code_stream.str());
+	response.WithString("body", body.View().WriteCompact());
+	return invocation_response::success(response.View().WriteCompact(), CONTENT_TYPE_JSON);	
     }
 
 }
