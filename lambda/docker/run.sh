b86b814ec6765f1e46073d8868a9933a1fd4c6e8
hi2k1978 <hi2k1978@gmail.com>
2023-05-06 17:48:04 +0900

    modify cmake

--- lambda/docker/run.sh
+++ lambda/docker/run.sh
@@ -14,9 +14,6 @@ mkdir -p build
 chown ${uid}:${gid} build
 cd build
 cmake ..
-# cmake --build .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/tmp/install
-# cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/tmp/install
-# make
 make ${target}
 cd ..
 chown -R ${uid}:${gid} build
