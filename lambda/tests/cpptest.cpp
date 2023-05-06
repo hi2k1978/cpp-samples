5fde4ccaf293b406d4ab40452413876fe2f59f2f
hi2k1978 <hi2k1978@gmail.com>
2023-05-06 19:06:48 +0900

    refactor cmake

--- lambda/tests/cpptest.cpp
+++ lambda/tests/cpptest.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "test_modules/TestModules.h"
+#include "TestModules/TestModules.h"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
